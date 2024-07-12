fe317fdbe9d14c7b6eea2e51b6e47600f382c5db
Gustavo Henrique Modesto Vasconcelos <gustavohenrique011921@gmail.com>
2024-07-12 20:18:30 -0300

    hotfixes

--- src/User.cpp
+++ src/User.cpp
@@ -2,14 +2,15 @@
 #include <iostream>       // Inclusão da biblioteca iostream para entrada e saída padrão
 #include <algorithm>      // Inclusão da biblioteca algorithm para entrada e saída padrão
 
-User::User(int id, const std::string &name, const std::string &email, const std::string &password)
-    : id(id), name(name), email(email), password(password) {} // Construtor da classe User que inicializa os atributos com os parâmetros fornecidos
+User::User(int id, const std::string &name, const std::string &email, const std::string &password, bool isAdmin)
+    : id(id), name(name), email(email), password(password), isAdmin(isAdmin){}
+      // Construtor da classe User que inicializa os atributos com os parâmetros fornecidos
 
 void User::addPlaylist(const MoviePlaylist &playlist) {
     playlists.push_back(playlist); // Adiciona uma playlist à lista de playlists do usuário
 }
 
-void User::displayMovies() const {
+void User::displayAddedMovies() const {
     std::cout << "User ID: " << id << "\nUser Name: " << name << "\nEmail: " << email << "\nMovies:" << std::endl; // Mostra informações básicas do usuário
     for (const auto &playlist : playlists) { // Para cada playlist do usuário
         playlist.display(); // Mostra os filmes da playlist
